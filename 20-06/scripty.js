4ac08c8d2f3edd0db2db78e5254b2a8fe177972b
Wyllgner <wyllgner_franca@hotmail.com>
2024-06-20 12:38:30 -0400

    20-06

---
+++ 20-06/scripty.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+$("#scrollHome").click(function(){
+    $("html, body").animate({
+        scrollTop: 1300
+    }, 500);
+});
+$("#scrollNews").click(function(){
+    $("html, body").animate({
+        scrollTop: 2300
+    }, 200);
+});
+
+$("#scrollContact").click(function(){
+    $("html, body").animate({
+        scrollTop: 3300
+    }, 200);
+});
+
+$("#scrollAbout").click(function(){
+    $("html, body").animate({
+        scrollTop: 4300
+    }, 200);
+});
+});
