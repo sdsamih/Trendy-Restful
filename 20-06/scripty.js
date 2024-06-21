c829e4644799112afdf17316db7671cd96205e5e
sdsamih <sdsamih@gmail.com>
2024-06-20 22:36:10 -0400

    aviso de formulario enviado funcionando minimamente

--- 20-06/scripty.js
+++ 20-06/scripty.js
@@ -4,6 +4,22 @@ $("#scrollHome").click(function(){
         scrollTop: 0
     }, 500);
 });
+
+const form = document.getElementById('formulario');
+
+    // Captura a caixa de mensagem
+    const messageBox = document.getElementById('messageBox');
+
+    // Adiciona um ouvinte de evento para o envio do formulário
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Previne o envio padrão do formulário
+
+        // Exibe a caixa de mensagem
+        messageBox.style.display = 'block';
+    });
+
+
+    
 $("#scrollNews").click(function(){
     $("html, body").animate({
         scrollTop: 686
